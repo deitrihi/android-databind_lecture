f5d8aab40523c396ed147ebde0280756e3cf2c94
deitrihi <deitrihi@naver.com>
2022-10-24 22:39:43 +0900

    init

--- app/src/main/java/com/deitrihi/databind_lecture/MainActivity.kt
+++ app/src/main/java/com/deitrihi/databind_lecture/MainActivity.kt
@@ -2,10 +2,21 @@ package com.deitrihi.databind_lecture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.deitrihi.databind_lecture.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btn.setOnClickListener {
+            Toast.makeText(this, "click", Toast.LENGTH_LONG).show()
+        }
     }
 }
